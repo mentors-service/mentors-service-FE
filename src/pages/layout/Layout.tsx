4effe7646cfb875707c41eb9d1b4dd1b36273ef8
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-10-12 16:36:04 +0900

    feat: 인가 코드 요청 구현

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,9 +1,18 @@
 import { Outlet } from 'react-router-dom';
 
+const KAKAO_OAUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT}&response_type=code`;
+
 const Layout = () => {
+  const handleClickLoginKakao = () => {
+    window.location.href = KAKAO_OAUTH_URL;
+  };
+
   return (
     <div>
       <header>header</header>
+      <button type='button' onClick={handleClickLoginKakao}>
+        카카오 로그인 버튼
+      </button>
 
       <main>
         <Outlet />
