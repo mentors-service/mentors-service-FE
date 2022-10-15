0c828ba77a6fb182b50329a3ff950dcb586ab4be
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-10-15 23:12:43 +0900

    feat: 기본 공통 스타일 환경 구성

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
+import theme from '@styles/theme';
 import App from './App';
 import { store } from './store/store';
 
@@ -13,12 +15,14 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/styles/constants/fonts.ts
@@ -0,0 +1,9 @@
+export type FontsTypes = typeof fonts;
+
+export const fonts = {
+  $xs: '14px',
+  $sm: '16px',
+  $base: '18px',
+  $lg: '20px',
+  $xl: '22px',
+};
