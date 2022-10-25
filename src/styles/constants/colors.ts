d962491db27f7a043ff30749af1a27c02b0ce591
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-10-25 20:56:35 +0900

    fix: 사용 color 변경

--- src/styles/constants/colors.ts
+++ src/styles/constants/colors.ts
@@ -3,6 +3,11 @@ export type ColorsTypes = typeof colors;
 export const colors = {
   $primary: '#FF055C',
   $secondary: '#0BBFAD',
+  $tertiary: '#FF055C',
+  $primary_dark: '#93B2BD',
+  $secondary_dark: '#088074',
+  $tertiary_dark: '#BF0445',
   $black: '#0D0D0D',
-  $white: '#F2F2F2',
+  $gray: '#EBEBEB',
+  $white: '#FFFFFFF',
 };
