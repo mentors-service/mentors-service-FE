a53b6704b9309670e8cd92fbfe332e9c6a066630
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-11-05 21:00:11 +0900

    modfiy: 제목, 장소, 태그 정규식 일부분 수정

--- src/pages/write/Write.tsx
+++ src/pages/write/Write.tsx
@@ -8,7 +8,7 @@ import MarkdownContext from '@hooks/contexts/markdownContext';
 import { ChangeEvent, useMemo, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { createArticle } from '@api/article';
-import { koreaCheck, FormErrorMessages, EnglishCheck, NumberCheck } from '@utils/formUtil';
+import { FormErrorMessages, NumberCheck } from '@utils/formUtil';
 import * as S from './Write.style';
 import { IFormInput } from './types';
 
@@ -47,56 +47,36 @@ const Write = () => {
         <input
           {...register('title', {
             required: true,
-            minLength: 3,
-            maxLength: 10,
-            pattern: koreaCheck,
           })}
           placeholder='제목 : 멘토링 모집'
           id='title'
           style={{ border: watch('title') && '2px solid #0BBFAD' }}
         />
-        {errors?.title?.type === 'pattern' && <S.ErrorMessage>{FormErrorMessages.NAME}</S.ErrorMessage>}
         {errors?.title?.type === 'required' && <S.ErrorMessage>{FormErrorMessages.NAME_REQUIRED}</S.ErrorMessage>}
-        {errors?.title?.type === 'maxLength' && <S.ErrorMessage>{FormErrorMessages.MAX_LENGTH}</S.ErrorMessage>}
-        {errors?.title?.type === 'minLength' && <S.ErrorMessage>{FormErrorMessages.MIN_LENGTH}</S.ErrorMessage>}
 
         <input
           {...register('place', {
             required: true,
-            minLength: 3,
-            maxLength: 10,
-            pattern: koreaCheck,
           })}
           placeholder='장소 : 서울, 인천'
           id='place'
           style={{ border: watch('place') && '2px solid #0BBFAD', color: watch('place') && '2px solid #0BBFAD' }}
         />
-        {errors?.place?.type === 'pattern' && <S.ErrorMessage>{FormErrorMessages.PLACE}</S.ErrorMessage>}
         {errors?.place?.type === 'required' && <S.ErrorMessage>{FormErrorMessages.PLACE_REQUIRED}</S.ErrorMessage>}
-        {errors?.place?.type === 'maxLength' && <S.ErrorMessage>{FormErrorMessages.MAX_LENGTH}</S.ErrorMessage>}
-        {errors?.place?.type === 'minLength' && <S.ErrorMessage>{FormErrorMessages.MIN_LENGTH}</S.ErrorMessage>}
 
         <input
           {...register('tag', {
             required: true,
-            minLength: 3,
-            maxLength: 20,
-            pattern: EnglishCheck,
           })}
           placeholder='태그 : Java , React'
           id='tag'
           style={{ border: watch('tag') && '2px solid #0BBFAD', color: watch('tag') && '2px solid #0BBFAD' }}
         />
-        {errors?.tag?.type === 'pattern' && <S.ErrorMessage>{FormErrorMessages.TAG}</S.ErrorMessage>}
         {errors?.tag?.type === 'required' && <S.ErrorMessage>{FormErrorMessages.TAG_REQUIRED}</S.ErrorMessage>}
-        {errors?.tag?.type === 'maxLength' && <S.ErrorMessage>{FormErrorMessages.MAX_LENGTH}</S.ErrorMessage>}
-        {errors?.tag?.type === 'minLength' && <S.ErrorMessage>{FormErrorMessages.MIN_LENGTH}</S.ErrorMessage>}
 
         <input
           {...register('schedule', {
             required: true,
-            minLength: 7,
-            maxLength: 25,
             pattern: NumberCheck,
           })}
           placeholder='일정 : 2022-01-01~2022-11-30'
@@ -107,8 +87,6 @@ const Write = () => {
         {errors?.schedule?.type === 'required' && (
           <S.ErrorMessage>{FormErrorMessages.SCHEDULE_REQUIRED}</S.ErrorMessage>
         )}
-        {errors?.schedule?.type === 'maxLength' && <S.ErrorMessage>{FormErrorMessages.MAX_LENGTH}</S.ErrorMessage>}
-        {errors?.schedule?.type === 'minLength' && <S.ErrorMessage>{FormErrorMessages.MIN_LENGTH}</S.ErrorMessage>}
 
         <div>
           <label htmlFor='인원수'>
--- src/utils/formUtil.ts
+++ src/utils/formUtil.ts
@@ -1,21 +1,10 @@
 export const FormErrorMessages = {
-  MAX_LENGTH: '너무 깁니다.',
-  MIN_LENGTH: '너무 짧습니다.',
-  NAME: '제목 형식에 맞지 않습니다.',
   NAME_REQUIRED: '제목은 필수입니다.',
-  PLACE: '장소 형식에 맞지 않습니다.',
   PLACE_REQUIRED: '장소는 필수입니다.',
-  TAG: '태그 형식에 맞지 않습니다.',
   TAG_REQUIRED: '태그는 필수입니다.',
   SCHEDULE: '일정 형식에 맞지 않습니다.',
   SCHEDULE_REQUIRED: '일정은 필수입니다.',
 };
-
-// 한글 검사
-export const koreaCheck = /[가-힣ㄱ-ㅎㅏ-ㅣ]/gi;
-
-// 영어 검사
-export const EnglishCheck = /[a-zA-Z]/;
 
 // 숫자 검사
 export const NumberCheck =
